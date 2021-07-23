1f843a8ca447b230efb3ad3a74b3adb2c19a3084
Mymii <63843105+Mymii@users.noreply.github.com>
2021-07-23 13:00:46 +0200

    update

--- pruebaJenkins.java
+++ pruebaJenkins.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 public class pruebaJenkins {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce una palabra: ");
-		String palabra = sc.nextLine();
+		String palabra = "";
 
 		System.out.println(palabra.toUpperCase());
 
