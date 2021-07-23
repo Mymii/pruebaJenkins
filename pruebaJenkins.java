a04b9c031ad1bc0a6da9fb20cd7e05692cc7a124
Mymii <63843105+Mymii@users.noreply.github.com>
2021-07-23 10:25:16 +0200

    added class

---
+++ pruebaJenkins.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class pruebaJenkins {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una palabra: ");
+		String palabra = sc.nextLine();
+
+		System.out.println(palabra.toUpperCase());
+
+	}
+
+}
