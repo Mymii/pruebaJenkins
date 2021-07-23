0e12d00b1cdccb60518783f1bd66a4a6486fcd0f
Mymii <63843105+Mymii@users.noreply.github.com>
2021-07-23 13:04:11 +0200

    test4

--- pruebaJenkins.java
+++ pruebaJenkins.java
@@ -1,7 +1,6 @@
 public class pruebaJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("Introduce una palabra: ");
 		String palabra=""; 
 palabra = palabra.toUpperCase();
 
